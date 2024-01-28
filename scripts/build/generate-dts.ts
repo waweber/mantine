d61395c56bd7f98c733ec25b212cb0b29e5c4ca7
Wes Weber <wweber@wesweber.com>
2024-01-28 14:59:08 -0500

    fix: make .d.mts files work with "nodenext" module resolution
    Update the generate-dts.ts build script to reference subpackages'
    index.js explicitly to conform with newer module resolution rules.

--- scripts/build/generate-dts.ts
+++ scripts/build/generate-dts.ts
@@ -6,8 +6,21 @@ export async function generateDts(packagePath: string) {
   await execa('yarn', ['tsc', '--project', path.join(packagePath, 'tsconfig.build.json')]);
 
   // Duplicate the type definitions for ESM
-  await fs.copy(
-    path.join(packagePath, 'lib/index.d.ts'),
-    path.join(packagePath, 'lib/index.d.mts')
-  );
+  await generateESMDts(packagePath);
+}
+
+// Rewrite relative imports to reference the index.js explicitly. This is
+// required to support TypeScript projects with "moduleResolution": "nodenext"
+// set, as these rules require relative imports to include the file extension
+// and do not support implicit index.js.
+// See https://github.com/mantinedev/mantine/issues/4991
+async function generateESMDts(packagePath: string) {
+  const explicitIndexRegex = /^export \* from '(\.\/.*)';$/gm;
+  const explicitIndexReplacement = 'export * from \'$1/index.js\'';
+  const inPath = path.join(packagePath, 'lib/index.d.ts');
+  const outPath = path.join(packagePath, 'lib/index.d.mts');
+
+  const dtsContent = (await fs.readFile(inPath)).toString();
+  const withExplicitIndex = dtsContent.replace(explicitIndexRegex, explicitIndexReplacement);
+  await fs.writeFile(outPath, withExplicitIndex);
 }
